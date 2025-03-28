d5238c54f346614d49ab0297066a972e3872ee56
Ronaldo Carvalho <ronaldocarvalho798@gmail.com>
2025-03-28 17:12:42 -0300

    Adiciona configuração CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors'); // Adicione esta linha
 
 dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
 
 const app = express();
+app.use(cors()); // Adicione esta linha
 const port = process.env.PORT || 3000;
 
 const usuario = process.env.USUARIO;
